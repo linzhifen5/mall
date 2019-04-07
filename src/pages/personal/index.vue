58cc59e911e8c95c379ae4b80a5ad236e3518b88
zhanghelin <1786524543@qq.com>
2019-04-08 00:02:00 +0800

    添加路由及重定向

---
+++ src/pages/personal/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Personal</div>
+</template>
+
+<script>
+export default {
+    name:'Personal'
+}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,43 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    
+  routes:[
+    {
+      name:'home',
+      path:'/home',
+      component:()=>import('pages/home'),
+      children:[
+        {
+          name:'home-product',
+          path:'product/:id',
+          component:()=>import('pages/product'),
+        }
+      ]
+    },
+    {
+      name:'category',
+      path:'/category',
+      component:()=>import('pages/category'),
+    },
+    {
+      name:'cart',
+      path:'/cart',
+      component:()=>import('pages/cart'),
+    },
+    {
+      name:'personal',
+      path:'/personal',
+      component:()=>import('pages/personal'),
+    },
+    {
+      name:'search',
+      path:'/search',
+      component:()=>import('pages/search'),
+    },
+    // 路由重定向
+    {
+      path:'*',
+      redirect:'/home'
+    },
   ]
 })
